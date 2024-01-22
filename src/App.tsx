2adcedd36b87eb9aa3ae852a35e2dee25be0dcbe
Namdurash <mshelper757@gmail.com>
2024-01-22 22:57:03 +0200

    [PLN-2] feat: Add splash screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {RootNavigator} from './navigation/RootNavigator';
 import {PaperProvider} from 'react-native-paper';
+import SplashScreen from 'react-native-splash-screen';
 
 function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <PaperProvider>
       <RootNavigator />
